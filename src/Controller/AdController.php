143143a6e1f76a00c50b608152d1afd2f8ec6cec
abenrejeb <ext-abenrejeb@leparisien.fr>
2022-11-23 12:50:28 +0100

    4. Add form Ad

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdController extends AbstractController
 {
@@ -23,6 +26,40 @@ class AdController extends AbstractController
 
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
+        ]);
+    }
+
+    /**
+     * Créer une annonce
+     *
+     * @Route("/ads/new", name="app_ads_new")
+     *
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $ad = new Ad();
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($ad);
+            $em->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+
+            return $this->redirectToRoute('app_ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
